5e485f5b321eaec13a94c574d330fd762d81a519
Seki <6431503002@lamduan.mfu.ac.th>
2022-04-25 00:13:44 +0700

    Test123

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,180 @@
+//====================================================================================================
+//The Free Edition of C# to Java Converter limits conversion output to 100 lines per file.
+
+//To purchase the Premium Edition, visit our website:
+//https://www.tangiblesoftwaresolutions.com/order/order-csharp-to-java.html
+//====================================================================================================
+
+import java.time.*;
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        //start
+
+        int member = 0;
+        int a = 0;
+        int room = 0;
+        do {
+            System.out.println(" Welcome to SE Hotel Reservation");
+            System.out.println("--------------------------------");
+            System.out.println(" Would you like to reserve your room?");
+            System.out.println(" 1.Yes or 2.No ");
+            System.out.println(" Please write only number!");
+            System.out.print(" You select: ");
+
+            int reserve = Integer.parseInt(new Scanner(System.in).nextLine());
+
+            if (reserve == 1) //reserve
+            {
+
+                System.out.println("--------------------------------");
+
+                System.out.println(" Enter your date of check-in");
+
+//                LocalDate checkin = LocalDate.parse(new Scanner(System.in).nextLine());
+
+                System.out.println(" Enter your date of check-out");
+//                LocalDate checkout = LocalDate.parse(new Scanner(System.in).nextLine());
+
+
+//                TimeSpan timesp = checkoutInt - checkinInt;
+
+
+                System.out.println("--------------------------------");
+                //member
+                System.out.print(" How many members?: ");
+                //chooseroom
+                member = Integer.parseInt(new Scanner(System.in).nextLine());
+
+
+                do {
+                    if (member <= 5) {
+                        String roomsize1 = " Smallroom";
+                        String roomsize2 = " Largeroom";
+                        System.out.println(" Please select room size");
+                        System.out.println(" 1." + roomsize1 + "or 2." + roomsize2);
+                        System.out.print(" You Choose :");
+                        room = Integer.parseInt(new Scanner(System.in).nextLine());
+
+
+                        switch (room) {
+                            case 1:
+                                String roomsize3 = " Smallroom";
+                                System.out.println(" Price 1/day for" + roomsize3 + "is $ 15000\n");
+
+                                break;
+
+                            case 2:
+                                String roomsize4 = " Largeroom";
+                                System.out.println(" Price 1/day for " + roomsize4 + " is $25000\n");
+
+
+                                break;
+                            default:
+                                System.out.println(" Select 1 or 2 Please try again\n");
+                                break;
+
+                        } //switch(room)
+
+
+                    } // if (member <= 5)
+
+                    else if (member >= 5 && member <= 10) {
+                        String roomsize5 = " Largeroom";
+                        System.out.println(" Your room  is Large room");
+                        System.out.println(" Price 1/day for " + roomsize5 + " is $25000\n");
+
+                    } //else if(member >= 5 && member <= 10)
+
+                    else {
+                        System.out.println(" *Over limit*");
+                    } //else
+                } while (room >= 3);
+
+                System.out.println(" --------------------------------");
+                System.out.println(" Your Reservation ");
+//                System.out.println(" Check - in :" + checkin);
+//                System.out.println(" Check - out :" + checkout);
+//                System.out.println(" Total day: " + (timesp.TotalDays));
+                System.out.println(" Total member :" + member);
+                if (room == 1) {
+
+//                    System.out.println(" Total price : $ " + (timesp.TotalDays) * 15000);
+                } else {
+
+//                    System.out.println(" Total price :  $ " + (timesp.TotalDays) * 25000);
+                }
+
+                if (room == 1) {
+                    System.out.println(" Room size : Small room");
+                } else {
+                    System.out.println(" Room size : Large room");
+                }
+
+                Namearray(member);
+
+            } //if
+            else if (reserve == 2) {
+
+                System.out.println(" Thank you ");
+
+            } //Else if
+
+            System.out.println(" --------------------------------");
+            System.out.println(String.format(" Would you like to reserve your room again?\nEnter 1 for Reservation\nEnter 2 for End"));
+            a = Integer.parseInt(new Scanner(System.in).nextLine());
+        } while (a != 2);
+        System.out.println(" Thank you");
+
+    } //Main
+
+    public static void Namearray(int member1) {
+
+        int[] phNum = new int[2];
+
+        String YN, Name;
+
+        System.out.println(" --------------------------------");
+        System.out.print(" Do you want to confirm\nYes or No:\n:");
+        YN = String.valueOf(new Scanner(System.in).nextLine());
+        String YN2 = YN.toUpperCase();
+
+        switch (YN2) {
+
+            case "YES":
+
+                System.out.print(" Please give your name\n:");
+                Name = String.valueOf(new Scanner(System.in).nextLine());
+
+                if (member1 >= 2) {
+                    System.out.println(" Please leave some of your phone number");
+                    for (int i = 0; i < phNum.length; i++) {
+                        System.out.print(" : ");
+                        phNum[i] = Integer.parseInt(new Scanner(System.in).nextLine());
+
+                    }
+
+                } else {
+                    System.out.println(" Please leave your phone number");
+                    for (int i = 1; i < phNum.length; i++)
+                    {
+//                        Console.Write(" : ");
+//                        phNum[i] = Convert.ToInt32(Console.ReadLine());
+
+                        System.out.println(" : ");
+                        phNum[i] = Integer.parseInt(new Scanner(System.in).nextLine());
+                    }
+
+
+                }
+
+                break;
+
+            case "NO" :
+                System.out.println(" Thank you for your attention");
+                break;
+        }//switchYN2
+    }//Namearray
+}//name
